52e4cbf905c5f7c68755a316596c19b35543b717
Bence Fábián <begnoc@gmail.com>
2020-12-07 18:00:28 +0100

    reading the xml works

--- Lib/Src/Pom.fs
+++ Lib/Src/Pom.fs
@@ -4,5 +4,22 @@ open FSharp.Data
 
 module Pom =
 
+    type PomProvider = XmlProvider<"../pom-example.xml">
+
+    let extractDependencies (project: PomProvider.Project): Dependency.t list =
+        let mkDep (dep: PomProvider.Dependency) = {
+            Dependency.groupId = dep.GroupId
+            Dependency.artifactId = dep.ArtifactId
+            Dependency.version = dep.Version
+            Dependency.scope = dep.Scope
+            Dependency.dType = dep.Type
+        }
+        project.Dependencies
+        |> Array.map mkDep
+        |> List.ofArray
+
     let parseString (contents: string): Dependency.t list =
-        []
+        PomProvider.Parse contents |> extractDependencies
+
+    let parseFile (path: string): Dependency.t list =
+        PomProvider.Load path |> extractDependencies
