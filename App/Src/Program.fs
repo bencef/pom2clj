2156de87c7d7dbcc1fb3087f6ed452f8828331ac
Bence Fábián <begnoc@gmail.com>
2020-12-12 12:54:27 +0100

    add command line handling

--- App/Src/Program.fs
+++ App/Src/Program.fs
@@ -1,7 +1,35 @@
 ﻿open System
 
+open FSharp.CommandLine
+open FSharp.CommandLine.Internals
+
 open Bencef.Pom2Boot.Lib
+
+let fileOption =
+    commandOption {
+        names ["f"; "file"]
+        description "The pom file to read"
+        takes (format("%s").withNames ["path-to-pom.xml"])
+    }
+
+let failOnFurtherArguments () =
+    command {
+        let! args = Abstraction.StateConfig.args
+        if not args.IsEmpty
+        then sprintf "unknown arguments: %A" args |> RequestShowHelp |> raise
+        else return ()
+    }
+
+let mainCommand () =
+    command {
+        opt file in fileOption |> CommandOption.zeroOrExactlyOne
+        failOnFurtherArguments ()
+        match file with
+        | Some file -> printfn "Got file: %s" file
+        | None      -> printfn "Got nothing"
+        return 0
+    }
 
 [<EntryPoint>]
 let main argv =
-    0
+    mainCommand() |> Command.runAsEntryPoint argv
